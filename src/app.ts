1af19babb4bb87e7e54d8b0f708cca6f2a2e4d3a
Harshvardhan Sharma <itshvsharma@gmail.com>
2025-02-24 22:57:30 +0530

    App file

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import express from "express";
-
+import paymentRoutes from "./routes/payment.route";
+import cors from "cors"
 const app = express();
 
 // Middlewares
 app.use(express.json());
+app.use(cors())
+
+app.get("/check", (req, res) => {
+  res.json({
+    MSG: "Working fine!",
+  });
+});
 
 // API Call
-
+app.use("/api/v1/payments/order", paymentRoutes);
 
 export { app };
